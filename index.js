26e1c61c9164846b459cb9f4ae117b717a8dd156
yassin <byounes.staiv@gmail.com>
2024-08-30 07:41:17 +0100

    few Changes

--- index.js
+++ index.js
@@ -1,52 +1,3 @@
-// const fs = require('fs');
-
-// const textIn = fs.readFileSync("./txt/input.txt",'utf-8');
-// console.log(textIn);
-
-// const textOut = `This is what we know about avocado : ${textIn} .\n Created : ${Date.now()}`;
-
-// fs.writeFileSync("./txt/output.txt", textOut);
-
-// console.log("Created");
-
-
-// fs.readFile("./txt/start.txt","utf-8",(err, data1)=>{
-//     fs.readFile(`./txt/${data1}.txt`,"utf-8",(err,data2)=>{
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt`,"utf-8",(err,data3)=>{
-//             const final = `${data2}\n${data3}😎`;
-//             fs.writeFile("./txt/final.txt",final,'utf-8',(err=>{
-//                 console.log("we write in the file sucsufully😛");
-//             }))
-//         })
-//     })
-// })
-// const http = require('http');
-// const url = require('url');
-
-// const server = http.createServer((req,res)=>{
-//     // console.log(req.url);
-//     // res.end("this is the server");
-//     const pathName = req.url;
-//     if(pathName === "/" || pathName === "/overview"){
-//         res.end("Welcome to the home page");
-//     }
-//     else if(pathName === "/younes"){
-//         res.end("Welcome to the your personal page");
-//     }
-//     else {
-//         res.writeHead(404,{'Content-Type':'text/html',
-//             'my-own-header':'hello'
-//         });
-//         res.end("<h1>error 404</h1>");
-//     }
-// });
-
-// server.listen("8000","127.0.0.1",()=>{
-//     console.log("server is running on port 8000");
-// })
-
-
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
@@ -87,6 +38,7 @@ const server = http.createServer((req,res)=>{
     }
     else if (pathName.includes){
         // let id = parseInt(pathName.split("id=")[1]);
+        //this ⬆ or this ⬇
         let id = parseInt(url.parse(pathName,true).query.id);
 
         // newData.map(ele => {
@@ -97,7 +49,7 @@ const server = http.createServer((req,res)=>{
         //         res.end(temp);
         //     }
         // })
-        
+        //this ⬆ or this ⬇
         let temp = replaceFunc(tempProduct,newData[id]);
         res.writeHead(200,{"content-type" : "text/html"});
         res.end(temp);
