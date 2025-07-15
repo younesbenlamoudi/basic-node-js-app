1d3c465924d473cca1cdf40b3351bf4e5f2151f9
yassin <byounes.staiv@gmail.com>
2025-07-15 10:43:39 +0100

    fix index

--- index.js
+++ index.js
@@ -56,6 +56,3 @@ const server = http.createServer((req,res)=>{
 server.listen("8000","127.0.0.1",()=>{
     console.log("server is running on port 8000");
 })
-
-maaaaaaaaaaaaaaaaaaamalmdjlk
-dkjflkdjkfljj
